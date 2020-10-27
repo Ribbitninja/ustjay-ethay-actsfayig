f30182310f3a9d37551e3cc5d7ca31e4f1b57dfd
Ribbitninja <60951520+Ribbitninja@users.noreply.github.com>
2020-10-26 20:32:31 -0700

    initial commit

--- main.py
+++ main.py
@@ -17,9 +17,25 @@ def get_fact():
     return facts[0].getText()
 
 
+def pig_latinize():
+    postHeaders = {
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Origin': 'https://hidden-journey-62459.herokuapp.com',
+        'Referer': 'https://hidden-journey-62459.herokuapp.com/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/86.0.4240.111 Safari/537.36'
+    }
+    payload = {"input_text": get_fact()}
+    session = requests.Session()
+    response = session.post(url="https://hidden-journey-62459.herokuapp.com/piglatinize/",
+                            data=payload, headers=postHeaders)
+    return response.url, '<br/><br/>' + payload["input_text"], response.text
+
+
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = pig_latinize()
+    return Response(response=fact, mimetype="text/html")
 
 
 if __name__ == "__main__":
